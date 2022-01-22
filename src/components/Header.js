e98a783608b75867b9498d46499fe0aa823caf6b
mohsenrs <mohsen8022@gmail.com>
2022-01-22 12:39:46 +0330

    toggle state added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiMenuAltRight } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'
 import classes from './header.module.scss'
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [size, setSize] = useState({ width: undefined, height: undefined })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false)
+    }
+  }, [size.width, menuOpen])
+
   return (
     <header className={classes.header}>
       <div className={classes.header__content}>
         <h2 className={classes.header__content__logo}>Navbar</h2>
 
-        <nav className={classes.header__content__nav}>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen ? classes.isMenu : ''
+          }`}
+        >
           <ul>
             <li>
               <a href='/'>pageOne</a>
@@ -25,7 +51,11 @@ function Header() {
         </nav>
 
         <div className={classes.header__content__toggle}>
-          <BiMenuAltRight />
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={() => setMenuOpen(true)} />
+          ) : (
+            <AiOutlineClose onClick={() => setMenuOpen(false)} />
+          )}
         </div>
       </div>
     </header>
